79a78b40925db3e8e564ce74bdd49071e4a43f66
iswenzz <suxlolz1528@gmail.com>
2019-11-22 19:31:27 +0100

    Moved ProcessMemoryReaderApi class

--- Utility/MemoryAPI.cs
+++ Utility/MemoryAPI.cs
@@ -4,59 +4,59 @@ using System.Runtime.InteropServices;
 
 namespace Iswenzz.CoD4.Utility
 {
-    /// <summary>
-    /// MemoryAPI made by https://www.youtube.com/channel/UCSnlTgoSWPybpnVgRPNp01w
-    /// </summary>
-    class ProcessMemoryReaderApi
-    {
-        [Flags]
-        public enum ProcessAccessType
-        {
-            PROCESS_TERMINATE = (0x0001),
-            PROCESS_CREATE_THREAD = (0x0002),
-            PROCESS_SET_SESSIONID = (0x0004),
-            PROCESS_VM_OPERATION = (0x0008),
-            PROCESS_VM_READ = (0x0010),
-            PROCESS_VM_WRITE = (0x0020),
-            PROCESS_DUP_HANDLE = (0x0040),
-            PROCESS_CREATE_PROCESS = (0x0080),
-            PROCESS_SET_QUOTA = (0x0100),
-            PROCESS_SET_INFORMATION = (0x0200),
-            PROCESS_QUERY_INFORMATION = (0x0400),
-            PROCESS_QUERY_LIMITED_INFORMATION = (0x1000)
-        }
-
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr OpenProcess
-        (
-            UInt32 dwDesiredAccess,
-            Int32 bInheritHandle,
-            UInt32 dwProcessId
-        );
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern Int32 WriteProcessMemory
-        (
-            IntPtr hProcess,
-            IntPtr lpBaseAddress,
-            [In, Out] byte[] lpBuffer,
-            UInt32 dwSize,
-            out IntPtr lpNumberOfBytesWritten
-        );
-
-        [DllImport("kernel32.dll")]
-        public static extern Int32 ReadProcessMemory
-        (
-            IntPtr hProcess,
-            IntPtr lpBaseAddress,
-            [In, Out] byte[] lpBuffer,
-            UInt32 dwSize,
-            out IntPtr lpNumberOfBytesRead
-        );
-    }
-
     public class ProcessMemoryReader
     {
+        /// <summary>
+        /// MemoryAPI made by https://www.youtube.com/channel/UCSnlTgoSWPybpnVgRPNp01w
+        /// </summary>
+        class ProcessMemoryReaderApi
+        {
+            [Flags]
+            public enum ProcessAccessType
+            {
+                PROCESS_TERMINATE = (0x0001),
+                PROCESS_CREATE_THREAD = (0x0002),
+                PROCESS_SET_SESSIONID = (0x0004),
+                PROCESS_VM_OPERATION = (0x0008),
+                PROCESS_VM_READ = (0x0010),
+                PROCESS_VM_WRITE = (0x0020),
+                PROCESS_DUP_HANDLE = (0x0040),
+                PROCESS_CREATE_PROCESS = (0x0080),
+                PROCESS_SET_QUOTA = (0x0100),
+                PROCESS_SET_INFORMATION = (0x0200),
+                PROCESS_QUERY_INFORMATION = (0x0400),
+                PROCESS_QUERY_LIMITED_INFORMATION = (0x1000)
+            }
+
+            [DllImport("kernel32.dll")]
+            public static extern IntPtr OpenProcess
+            (
+                UInt32 dwDesiredAccess,
+                Int32 bInheritHandle,
+                UInt32 dwProcessId
+            );
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            public static extern Int32 WriteProcessMemory
+            (
+                IntPtr hProcess,
+                IntPtr lpBaseAddress,
+                [In, Out] byte[] lpBuffer,
+                UInt32 dwSize,
+                out IntPtr lpNumberOfBytesWritten
+            );
+
+            [DllImport("kernel32.dll")]
+            public static extern Int32 ReadProcessMemory
+            (
+                IntPtr hProcess,
+                IntPtr lpBaseAddress,
+                [In, Out] byte[] lpBuffer,
+                UInt32 dwSize,
+                out IntPtr lpNumberOfBytesRead
+            );
+        }
+
         public Process ReadProcess { get; set; }
         private IntPtr handle;
 
